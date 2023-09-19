24ab65a9db348625ffd893f7d531747edd91aca2
Moritz Zschunke <moritz_z17@web.de>
2023-09-19 19:43:21 +0200

    added random button/image

--- src/app/page.js
+++ src/app/page.js
@@ -3,6 +3,7 @@ import Search from "../../components/Search";
 import styles from "./Homepage.module.css";
 import Image from "next/image";
 import recipe from "../../lib/recipe.png";
+import dices from "../../lib/dices.png";
 
 export default function Home() {
   return (
@@ -11,18 +12,28 @@ export default function Home() {
         <h1 className={styles["title"]}>Recipe Buddy</h1>
       </div>
       <div className={styles["menu-container"]}>
-        <Link href="/recipes">
+        <Link href="/recipes" className={styles["link"]}>
           <Image
             src={recipe}
             style={{ objectFit: "contain" }}
-            width={40}
-            height={40}
+            width={35}
+            height={35}
             alt="Picture of my recipes"
           ></Image>
-          Go to my recipes
+          <p className={styles["link-text"]}>My recipes</p>
         </Link>
-        <Search />
+        <button className={styles["button"]}>
+          <Image
+            src={dices}
+            style={{ objectFit: "contain" }}
+            width={35}
+            height={35}
+            alt="Picture of dices"
+          ></Image>
+          <p className={styles["link-text"]}>Random meal</p>
+        </button>
       </div>
+      <Search />
     </>
   );
 }
