389b16ce15c2030b932d55e46f982d8f403858df
Moritz Zschunke <moritz_z17@web.de>
2023-09-19 19:55:32 +0200

    added image to home button

--- src/app/recipes/page.js
+++ src/app/recipes/page.js
@@ -1,13 +1,23 @@
 import Link from "next/link";
 import RecipeList from "../../../components/RecipeList";
 import styles from "./RecipePage.module.css";
+import Image from "next/image";
+import home from "../../../lib/home.png";
 
 export default async function RecipePage() {
   return (
     <>
-      <Link href="/">Go Home</Link>
+      <Link href="/" className={styles["home-button"]}>
+        <Image
+          src={home}
+          style={{ objectFit: "contain" }}
+          width={35}
+          height={35}
+          alt="Picture of my recipes"
+        ></Image>
+      </Link>
       <RecipeList />
-      <Link href="/recipes/create" className={styles["button"]}>
+      <Link href="/recipes/create" className={styles["add-button"]}>
         add recipe
       </Link>
     </>
