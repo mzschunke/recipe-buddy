f7cb2c0de0e282136c91dee0a8cd2156085d635c
Moritz Zschunke <moritz_z17@web.de>
2023-09-19 19:04:45 +0200

    strutured homepage & added recipe img

--- src/app/page.js
+++ src/app/page.js
@@ -1,12 +1,28 @@
 import Link from "next/link";
 import Search from "../../components/Search";
+import styles from "./Homepage.module.css";
+import Image from "next/image";
+import recipe from "../../lib/recipe.png";
 
 export default function Home() {
   return (
     <>
-      <h1>Welcome to the ultimate Recipe App</h1>
-      <Link href="/recipes">My recipes</Link>
-      <Search />
+      <div className={styles["header-container"]}>
+        <h1 className={styles["title"]}>Recipe Buddy</h1>
+      </div>
+      <div className={styles["menu-container"]}>
+        <Link href="/recipes">
+          <Image
+            src={recipe}
+            style={{ objectFit: "contain" }}
+            width={40}
+            height={40}
+            alt="Picture of my recipes"
+          ></Image>
+          Go to my recipes
+        </Link>
+        <Search />
+      </div>
     </>
   );
 }
