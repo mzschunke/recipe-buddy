7a1fc8fd5acba1e64f196c4c55b363a3052d5025
Moritz Zschunke <moritz_z17@web.de>
2023-09-20 09:26:25 +0200

    refactor: created MyRecipes button component

--- src/app/page.js
+++ src/app/page.js
@@ -1,9 +1,8 @@
-import Link from "next/link";
 import Search from "../../components/Search";
 import styles from "./Homepage.module.css";
 import Image from "next/image";
-import recipe from "../../lib/recipe.png";
 import dices from "../../lib/dices.png";
+import RecipesButton from "../../components/MyRecipesButton";
 
 export default function Home() {
   return (
@@ -12,16 +11,7 @@ export default function Home() {
         <h1 className={styles["title"]}>Recipe Buddy</h1>
       </div>
       <div className={styles["menu-container"]}>
-        <Link href="/recipes" className={styles["link"]}>
-          <Image
-            src={recipe}
-            style={{ objectFit: "contain" }}
-            width={35}
-            height={35}
-            alt="Picture of my recipes"
-          ></Image>
-          <p className={styles["link-text"]}>My recipes</p>
-        </Link>
+        <RecipesButton />
         <button className={styles["button"]}>
           <Image
             src={dices}
