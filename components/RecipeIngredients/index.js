22738cebc6e1727a6f404d50021363fedfa35c6e
Moritz Zschunke <moritz_z17@web.de>
2023-10-09 20:46:11 +0200

    added ingredient array to schema for better conciseness

--- components/RecipeIngredients/index.js
+++ components/RecipeIngredients/index.js
@@ -1,14 +1,6 @@
 import styles from "./RecipeIngredients.module.css";
 
 export default function RecipeIngredients({ recipe, customStyles }) {
-  const ingredientProperties = [];
-  for (let i = 1; i <= 20; i++) {
-    const ingredient = recipe[`strIngredient${i}`];
-    const measure = recipe[`strMeasure${i}`];
-    if (ingredient && ingredient.trim() !== "") {
-      ingredientProperties.push({ ingredient, measure });
-    }
-  }
   return (
     <div
       className={`${styles["ingredients-container"]} ${customStyles?.container}`}
@@ -20,7 +12,7 @@ export default function RecipeIngredients({ recipe, customStyles }) {
         className={`${styles["ingredients-container2"]} ${customStyles?.container2}`}
       >
         <ul>
-          {ingredientProperties.map((item, index) => (
+          {recipe.ingredients.map((item, index) => (
             <li key={index}>
               {item.measure} {item.ingredient}
             </li>
--- utilities/favorite.js
+++ utilities/favorite.js
@@ -1,11 +1,25 @@
 async function addToFavorites(recipe) {
   try {
+    const ingredients = [];
+
+    for (let i = 1; i <= 20; i++) {
+      const ingredientName = recipe[`strIngredient${i}`];
+      const ingredientMeasure = recipe[`strMeasure${i}`];
+      if (ingredientName && ingredientMeasure) {
+        ingredients.push({
+          ingredient: ingredientName,
+          measure: ingredientMeasure,
+        });
+      }
+    }
+
     const recipeData = {
       strMeal: recipe.strMeal,
       strCategory: recipe.strCategory,
       strArea: recipe.strArea,
       strMealThumb: recipe.strMealThumb,
       strInstructions: recipe.strInstructions,
+      ingredients: ingredients,
     };
 
     const response = await fetch("/api/recipes", {
