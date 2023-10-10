1ec41108d333841a93f59befd145ec1fb14d3c52
Moritz Zschunke <moritz_z17@web.de>
2023-10-10 11:22:56 +0200

    adjusted home icon

--- components/HomeButton/index.js
+++ components/HomeButton/index.js
@@ -1,19 +1,12 @@
 import Link from "next/link";
-import Image from "next/image";
-import home from "../../lib/home.png";
 import styles from "./HomeButton.module.css";
+import { BiSolidHome } from "react-icons/bi";
 
 export default async function HomeButton() {
   return (
     <>
       <Link href="/" className={styles["home-button"]}>
-        <Image
-          src={home}
-          style={{ objectFit: "contain" }}
-          width={25}
-          height={25}
-          alt="Home Button"
-        ></Image>
+        <BiSolidHome size={30} color="green" />
       </Link>
     </>
   );
